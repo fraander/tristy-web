d2879b5793036c98f97da8ada29e3a0aaad01aa6
fraander <57777918+fraander@users.noreply.github.com>
2023-10-04 21:45:31 -0400

    got auth going for real

--- lib/initSupabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://aoemfzszzwmseyjkgtxe.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || ""; // in .env.local NEXT_PUBLIC_SUPABASE_KEY = "..."
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-export { supabase };
